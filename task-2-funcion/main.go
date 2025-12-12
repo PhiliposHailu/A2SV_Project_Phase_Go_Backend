43401e632715db5ad4966cec098e6ae77e0ec3ee
PhiliposHailu <hailuphilipos@gmail.com>
2025-12-12 12:41:42 +0300

    Task 2 completed

--- task-2-funcion/main.go
+++ task-2-funcion/main.go
@@ -5,14 +5,14 @@ import (
 	"unicode"
 )
 
-func dictionary(s string) map[string]int {
-	myRune := []rune(s)
+func Dictionary(s string) map[string]int {
+	runeS := []rune(s)
 	wordMap := map[string]int{}
 	L := 0
-	for r := range myRune {
-		char := myRune[r]
+	for r := range runeS {
+		char := runeS[r]
 		if !unicode.IsLetter(char) {
-			word := strings.ToLower(string(myRune[L:r]))
+			word := strings.ToLower(string(runeS[L:r]))
 			if len(word) > 0 {
 				wordMap[word] += 1
 			}
@@ -20,10 +20,39 @@ func dictionary(s string) map[string]int {
 		}
 
 	}
-	if L < len(myRune) {
-		word := strings.ToLower(string(myRune[L:]))
+	if L < len(runeS) {
+		word := strings.ToLower(string(runeS[L:]))
 		wordMap[word] += 1
 	}
 	return wordMap
 }
 
+func Palindrom(s string) bool {
+	runeS := []rune(s)
+	L := 0
+	r := len(runeS) - 1
+
+	for L < r {
+		for L < r && !unicode.IsLetter(runeS[L]) && !unicode.IsNumber(runeS[L]) {
+			L++
+		}
+
+		for L < r && !unicode.IsLetter(runeS[r]) && !unicode.IsNumber(runeS[r]) {
+			r--
+		}
+
+		if L < r {
+			char1 := strings.ToLower(string(runeS[L])) 
+			char2 := strings.ToLower(string(runeS[r]))
+
+			if char1 != char2 {
+				return false
+			}
+	
+			L++
+			r--
+		} 
+	}
+
+	return true
+}
--- task-2-funcion/main_test.go
+++ task-2-funcion/main_test.go
@@ -13,23 +13,23 @@ func TestDictionary(t *testing.T) {
 		expected map[string]int
 	}{
 		{
-			name: "Empty stirng",
+			name:     "Empty stirng",
 			input:    "",
 			expected: map[string]int{},
 		},
 
 		{
-			name: "Complex test",
+			name:  "Complex test",
 			input: "solo_dolo,solo,Solo,dolo,Dolo-so-so-so_SO,So010101",
 			expected: map[string]int{
 				"solo": 3,
 				"dolo": 3,
-				"so": 5,
+				"so":   5,
 			},
 		},
 
 		{
-			name: "Numbers",
+			name:  "Numbers",
 			input: "123556879%#&H^^*(*)(*&^%h2H8l3L5O06a2)",
 			expected: map[string]int{
 				"h": 3,
@@ -40,7 +40,7 @@ func TestDictionary(t *testing.T) {
 		},
 
 		{
-			name: "Complex test",
+			name:  "Complex test",
 			input: "solo_dolo,solo,Solo",
 			expected: map[string]int{
 				"solo": 3,
@@ -52,9 +52,63 @@ func TestDictionary(t *testing.T) {
 	for _, tt := range table {
 		t.Run(tt.name, func(t *testing.T) {
 
-			got := dictionary(tt.input)
-			if  !maps.Equal(tt.expected, got){
+			got := Dictionary(tt.input)
+			if !maps.Equal(tt.expected, got) {
 				t.Errorf("Falied\ngot: %v\nexpected: %v", got, tt.expected)
+			}
+		})
+	}
+}
+
+func TestPalindrom(t *testing.T) {
+	// create table for multipele test cases
+	table := []struct {
+		name      string
+		input     string
+		exptected bool
+	}{
+		{
+			name:      "Word",
+			input:     "carrac",
+			exptected: true,
+		},
+
+		{
+			name:      "Numbers",
+			input:     "123454321",
+			exptected: true,
+		},
+
+		{
+			name:      "Not True",
+			input:     "1234a54321",
+			exptected: false,
+		},
+
+		{
+			name:      "Complex",
+			input:     "12ab -b a 2 1",
+			exptected: true,
+		},
+
+		{
+			name:      "Unique",
+			input:     "-*&^%#$%^&*()_}:",
+			exptected: true,
+		},
+
+		{
+			name:      "EdgesCase",
+			input:     "AB121Ba",
+			exptected: true,
+		},
+	}
+
+	for _, tt := range table {
+		t.Run(tt.name, func(t *testing.T){
+			got := Palindrom(tt.input)
+			if tt.exptected != got {
+				t.Errorf("Falied \nExpected: %v\nGot: %v", tt.exptected, got)
 			}
 		})
 	}
