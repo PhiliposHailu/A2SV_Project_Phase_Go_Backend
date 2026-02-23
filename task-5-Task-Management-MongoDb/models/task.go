c786bc98e776ffa243c8c6dec6098cb03a0dec44
PhiliposHailu <hailuphilipos@gmail.com>
2026-02-23 11:32:21 +0300

    feat: demo CRUD opeations on MongoDb

--- task-5-Task-Management-MongoDb/models/task.go
+++ task-5-Task-Management-MongoDb/models/task.go
@@ -1,7 +1,9 @@
 package models
 
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
 type Task struct {
-	ID          int    `json:"id" bson:"_id,omitempty"`
+	ID          primitive.ObjectID    `json:"id" bson:"_id,omitempty"`
 	Title       string `json:"title" bson:"title"`
 	Description string `json:"description" bson:"description"`
 	DueDate     string `json:"due_date" bson:"due_date"`
